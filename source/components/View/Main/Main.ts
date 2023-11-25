fa9c5e8216c5f602ee7776c0cb12bbaac51c5397
Junior Programmer <progphyton@gmail.com>
2023-11-25 12:53:53 +0300

    feat: add child element to main component

--- source/components/View/Main/Main.ts
+++ source/components/View/Main/Main.ts
@@ -5,6 +5,11 @@ import * as styles from './main.module.css';
 export const Main: MainType = () => {
   const MAIN: HTMLElement = document.createElement('main');
   MAIN.className = <string>styles['main'];
-  MAIN.textContent = 'MAIN MODULE';
+  const CHILD = `
+    <div>
+        <h2>Welcome to Open Movie!</h2>
+    </div>
+  `;
+  MAIN.innerHTML = CHILD;
   return MAIN;
 };
