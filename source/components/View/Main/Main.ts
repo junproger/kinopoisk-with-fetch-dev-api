6f5c512c9ff298374071c16c9b6a809c04c11730
Junior Programmer <progphyton@gmail.com>
2023-11-25 18:41:36 +0300

    feat: add Item and Loader to parent components

--- source/components/View/Main/Main.ts
+++ source/components/View/Main/Main.ts
@@ -1,5 +1,6 @@
 import { Conponent } from '../../../typing/Component';
 
+import { Loader } from '../Loader/Loader';
 import { Movies } from '../Movies/Movies';
 
 import * as styles from './main.module.css';
@@ -13,6 +14,10 @@ export const Main: Conponent = () => {
     </div>
   `;
   MAIN.innerHTML = CHILD;
-  MAIN.append(Movies());
+  MAIN.append(Loader());
+  setTimeout(() => {
+    MAIN.lastChild?.remove();
+    MAIN.append(Movies());
+  }, 4000);
   return MAIN;
 };
--- source/components/View/Movies/Movies.ts
+++ source/components/View/Movies/Movies.ts
@@ -1,10 +1,12 @@
 import { Conponent } from '../../../typing/Component';
+import { Item } from '../Item/Item';
 
 import * as styles from './movies.module.css';
 
 export const Movies: Conponent = () => {
   const MOVIES: HTMLElement = document.createElement('div');
   MOVIES.classList.add(<string>styles['movies'], <string>styles['grids']);
-  MOVIES.textContent = 'MOVIES MODULE';
+  const LIST = new Array(16).fill(0);
+  LIST.map(() => MOVIES.append(Item()));
   return MOVIES;
 };
