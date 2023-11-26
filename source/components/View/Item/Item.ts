2d24e2fe9a13bc64ab2c18f7de1dd7652850cdfa
Junior Programmer <progphyton@gmail.com>
2023-11-26 15:25:42 +0300

    feat: add movie title and movie poster to Item

--- source/components/View/Item/Item.ts
+++ source/components/View/Item/Item.ts
@@ -1,10 +1,20 @@
 import { Conponent } from '../../../typing/Component';
 
+import poster from '../../../assets/barbenheimer.webp';
+
 import * as styles from './item.module.css';
 
 export const Item: Conponent = () => {
-  const ITEM: HTMLElement = document.createElement('div');
+  const ITEM: HTMLElement = document.createElement('section');
   ITEM.className = <string>styles['item'];
-  ITEM.textContent = 'MOVIE ITEM';
+  const IMG: HTMLImageElement = document.createElement('img');
+  IMG.className = <string>styles['poster'];
+  IMG.alt = 'poster';
+  IMG.src = poster;
+  ITEM.append(IMG);
+  const H3: HTMLHeadingElement = document.createElement('h3');
+  H3.className = <string>styles['title'];
+  H3.textContent = 'MOVIE TITLE';
+  ITEM.append(H3);
   return ITEM;
 };
