24d5775eef4159b6da333eda6ce45557e1d809f6
Junior Programmer <progphyton@gmail.com>
2023-11-26 13:39:30 +0300

    refactor: divide header title with three elements span

--- source/components/View/Header/Header.ts
+++ source/components/View/Header/Header.ts
@@ -7,7 +7,13 @@ export const Header: Conponent = () => {
   HEADER.className = <string>styles['header'];
   const H1: HTMLHeadingElement = document.createElement('h1');
   H1.className = <string>styles['title'];
-  H1.textContent = 'OPEN MOVIES PORTAL';
+  const SPAN1: HTMLSpanElement = document.createElement('span');
+  SPAN1.textContent = 'OPEN';
+  const SPAN2: HTMLSpanElement = document.createElement('span');
+  SPAN2.textContent = 'MOVIES';
+  const SPAN3: HTMLSpanElement = document.createElement('span');
+  SPAN3.textContent = 'PORTAL';
+  H1.append(SPAN1, SPAN2, SPAN3);
   HEADER.append(H1);
   return HEADER;
 };
