de1f33482f2ed13f3e9bd7fbdbb9b7ad68fc3eb3
Junior Programmer <progphyton@gmail.com>
2023-11-26 20:02:59 +0300

    refactor: join form attributes with autocomplete and aria-label

--- source/components/Control/Form/Form.ts
+++ source/components/Control/Form/Form.ts
@@ -4,22 +4,32 @@ import * as styles from './form.module.css';
 
 export const Form: Conponent = () => {
   const FORM: HTMLFormElement = document.createElement('form');
+  FORM.setAttribute('name', 'joinform');
+  FORM.setAttribute('aria-label', 'join form');
   FORM.className = <string>styles['form'];
   const NAME = Object.assign(document.createElement('input'), {
     type: 'text',
+    name: 'name',
+    autocomplete: 'off',
     className: `${<string>styles['name']}`,
     placeholder: 'YOUR NAME',
+    ariaLabel: 'Input Name',
   });
   FORM.append(NAME);
   const TEL = Object.assign(document.createElement('input'), {
     type: 'tel',
+    name: 'phone',
+    autocomplete: 'off',
     className: `${<string>styles['phone']}`,
     placeholder: '+7 (123) 456-78-90',
+    ariaLabel: 'Input Phone',
   });
   FORM.append(TEL);
   const BTN = Object.assign(document.createElement('input'), {
     type: 'submit',
+    name: 'button',
     className: `${<string>styles['button']}`,
+    ariaLabel: 'Submit',
     value: 'JOIN',
   });
   FORM.append(BTN);
