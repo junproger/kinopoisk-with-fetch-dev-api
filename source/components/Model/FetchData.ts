c4026609475499dd83f69b383782108fffe8bfc6
Junior Programmer <progphyton@gmail.com>
2023-11-27 07:48:33 +0300

    fix: add accept header to headrs in request

--- source/components/Model/FetchData.ts
+++ source/components/Model/FetchData.ts
@@ -6,7 +6,7 @@ export const FetchData = (callback: (data: DefaultRespType) => void): Promise<De
   const REQUEST = 'https://api.kinopoisk.dev/v1.4/movie?rating.imdb=8-10&limit=32&page=1';
   const FETCHING = fetch(REQUEST, {
     method: 'GET',
-    headers: { 'X-API-KEY': 'P3RTS9G-2YH4XAV-QWKVWAZ-E9XFFDQ' },
+    headers: { 'X-API-KEY': 'P3RTS9G-2YH4XAV-QWKVWAZ-E9XFFDQ', Accept: 'application/json' },
   })
     .then((resp) => resp.json())
     .then((data: DefaultRespType) => {
