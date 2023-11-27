acb2a41702c255921b83d8e01a3fdba56a804122
Junior Programmer <progphyton@gmail.com>
2023-11-27 17:25:43 +0300

    refactor: create Img element with object assign

--- source/components/View/Item/Item.ts
+++ source/components/View/Item/Item.ts
@@ -6,11 +6,12 @@ export const Item: ConponentWithProp = (prop) => {
   const [name, prev] = prop;
   const ITEM: HTMLElement = document.createElement('section');
   ITEM.className = <string>styles['item'];
-  const IMG: HTMLImageElement = document.createElement('img');
-  IMG.className = <string>styles['poster'];
-  IMG.src = <string>prev;
-  IMG.title = `${name}`;
-  IMG.alt = `${name}`;
+  const IMG = Object.assign(document.createElement('img'), {
+    className: <string>styles['poster'],
+    src: <string>prev,
+    title: `${name}`,
+    alt: `${name}`,
+  });
   ITEM.append(IMG);
   const H3: HTMLHeadingElement = document.createElement('h3');
   H3.className = <string>styles['title'];
