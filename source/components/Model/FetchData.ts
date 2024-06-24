6f06333c9f24dc8ba56975441c04920f8512f28f
Junior Programmer <progphyton@gmail.com>
2024-06-24 10:59:33 +0300

    fix: error with poster undefined value

--- source/components/Model/FetchData.ts
+++ source/components/Model/FetchData.ts
@@ -3,13 +3,17 @@ import { DefaultRespType } from '../../typing/DefaultRespType';
 import { logging } from '../../utils/logging';
 
 export const FetchData = (callback: (data: DefaultRespType) => void): Promise<DefaultRespType> => {
-  const REQUEST = 'https://api.kinopoisk.dev/v1.4/movie?rating.imdb=8-10&limit=32&page=1';
+  const REQUEST = 'https://api.kinopoisk.dev/v1.4/movie?page=1&limit=32&notNullFields=poster.url&rating.imdb=8-10';
   const FETCHING = fetch(REQUEST, {
     method: 'GET',
     headers: { 'X-API-KEY': 'P3RTS9G-2YH4XAV-QWKVWAZ-E9XFFDQ', Accept: 'application/json' },
   })
-    .then((resp) => resp.json())
+    .then((resp) => {
+      // logging(resp);
+      return resp.json();
+    })
     .then((data: DefaultRespType) => {
+      // logging(data);
       callback(data);
       return data;
     })
